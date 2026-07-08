5e69d87763bfacc8c09d6aa4f017e3c80840ed0b
aveesah03 <saveesah@gmail.com>
2026-07-08 22:45:52 +0530

    Added correct client side URL in CORS

--- server/src/index.ts
+++ server/src/index.ts
@@ -15,7 +15,7 @@ app.use(
   cors({
     origin: [
       "http://localhost:5173",
-      "https://your-app.vercel.app",
+      "https://ai-workout-planner-sepia.vercel.app/",
     ],
     credentials: true,
   })
